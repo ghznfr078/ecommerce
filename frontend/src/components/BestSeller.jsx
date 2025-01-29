bf83416a221533063cf05a3e89faee7764f71f9e
ghazanfar <ghznfr0078@gmail.com>
2025-01-29 12:52:08 +0500

    Add BestSeller and OurPolicy components; update Home page layout and styles

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,32 +3,31 @@ import { useShop } from "../context/ShopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useShop();
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
-
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Restart the development server after adding global styles. Make sure
-          Tailwind v4 is correctly installed.
+          Your issue likely arises because Tailwind applies styles using utility
+          classes, ternal stylesheets by default. Here are a few solutions:
         </p>
       </div>
 
-      {/* Rendering products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item._id}
-            image={item.image}
             name={item.name}
+            image={item.image}
             price={item.price}
           />
         ))}
@@ -37,4 +36,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
